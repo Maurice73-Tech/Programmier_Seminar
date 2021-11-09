1e16391faca4c1e00133318d9bbc9ca4ebc622c8
Daan Koser <daankoser@gmx.de>
2021-11-09 11:10:46 +0100

    login Seite logik

--- djangoGlobal/app_1/views.py
+++ djangoGlobal/app_1/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
-# Create your views here.
-#test ronny
+def login(request):
+    form = loginSeite()
+    return render(request,'app_1/login.html',{'form:':form})
+def logout(request):
+    form = logoutSeite()
+    return render(request,'app_1/logout.html',{'form:':form})
+def landing(request):
+    return render(request,'app_1/landing.html')
