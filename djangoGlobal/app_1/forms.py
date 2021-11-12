dfca87912769e6e89b6113fa23a35bc681300f6e
timonc98 <timon-clauss@gmx.de>
2021-11-12 02:32:06 +0100

    admin verändert

--- djangoGlobal/app_1/forms.py
+++ djangoGlobal/app_1/forms.py
@@ -3,6 +3,9 @@ from django.forms import fields
 from django import forms
 
 class Registrierungsform(forms.ModelForm):
+    vorname = forms.CharField (label="Vorname")
+    nachname = forms.CharField (label="Nachname")
+    abteilung = forms.CharField (label="Abteilung")
     class Meta:
         model= User
-        fields = ('__all__')
+        fields = ('vorname', 'nachname','abteilung','username', 'password','password1',)
