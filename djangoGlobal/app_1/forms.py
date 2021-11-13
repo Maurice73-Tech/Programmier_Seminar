8fb323a2ad46beec36b924fa4c0b20bea1845af4
Maurice <maurice.neidhart97@gmail.com>
2021-11-13 13:39:57 +0100

    new changes

--- djangoGlobal/app_1/forms.py
+++ djangoGlobal/app_1/forms.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.forms import fields
 from django import forms
-from .models import Post
 
 class Registrierungsform(forms.ModelForm):
     vorname = forms.CharField (label="Vorname")
