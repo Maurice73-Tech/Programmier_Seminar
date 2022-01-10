63e221ec3a451d0f71a99c74e77902d2101f3177
timonc98 <timon-clauss@gmx.de>
2022-01-10 10:29:04 +0100

    Admin Centen Anpassung

--- djangoGlobal/app_1/admin.py
+++ djangoGlobal/app_1/admin.py
@@ -5,21 +5,29 @@ from app_1.models import NeueBenutzer
 from django.contrib.auth.admin import UserAdmin
 from .models import Post, Kommentar ,UnterKommentar
 
-
-admin.site.register(Post)
-admin.site.register(Kommentar)
-admin.site.register(UnterKommentar)
 # Admin Seite konfiguriert: Attribute angeordnet und unwichtige ausgeblendet
 class UserAdminKonfig (UserAdmin):
     search_fields = ('username', 'vorname', 'nachname', 'abteilung')
     list_filter = ('username', 'vorname', 'nachname', 'abteilung', 'is_staff', 'is_active')
     ordering = ('username',)
-    list_display =('username', 'vorname', 'nachname', 'abteilung')
+    list_display =('username', 'vorname', 'nachname', 'email', 'abteilung')
 
     fieldsets =(
-        (None, {'fields':('username', 'vorname', 'nachname', 'abteilung', 'geburtsdatum',)}),
+        (None, {'fields':('username', 'vorname', 'nachname', 'abteilung', 'geburtsdatum','email',)}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
 
     )
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('Titel', 'Inhalt', 'Autor', 'Postdatum')
+
+@admin.register(Kommentar)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('post', 'name', 'content', 'date_added')
+
+@admin.register(UnterKommentar)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('post', 'name', 'content', 'parent')
+
 
 admin.site.register (NeueBenutzer,UserAdminKonfig)
